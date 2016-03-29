68f2896b159d6922dd234fb89c2f08fd0395e69f
Kiwon Yun <kiwonyun@stonybrook.edu>
2016-03-29 19:47:58 -0400

    User VideoReader instead of mmread on linux

--- experiment_setup/experiment_builder/src/generate_video_info_file.m
+++ experiment_setup/experiment_builder/src/generate_video_info_file.m
@@ -1,14 +1,13 @@
-clear all;
-close all;
+clear;
 clc;
 
 addpath( genpath('mmread') );
 
 video_root_path = '../data/output';
 
-files = dir(fullfile(video_root_path, '/*.mp4'));
+files = dir(fullfile(video_root_path, '/*.avi'));
 
-%% check whether filename contains an unsupported character
+%  check whether filename contains an unsupported character
 disp('remove "-" from the name of the files\n');
 
 fout = fopen('filename_info.txt', 'w');
@@ -26,7 +25,7 @@ end
 
 fclose(fout);
 
-%% save the video information
+%  save the video information
 width = [];
 height = [];
 totalDuration = [];
@@ -38,14 +37,28 @@ for i=1:length(files)
     filename = files(i).name;
     fprintf('%s is processing...(%d/%d)\n', filename, i, length(files));
     
-    video = mmread(sprintf('%s/%s', video_root_path, filename), [], [], false, true);
+    if ispc
+        video = mmread(sprintf('%s/%s', video_root_path, filename), [], [], false, true);
+        
+        video.Width = video.width;
+        video.Height = video.height;
+        video.Duration = video.totalDuration;
+        video.FrameTotal = video.nrFramesTotal;
+        video.FrameRate = video.rate;
+    else
+        % NOTE: the following does also not work on Matlab 2014b in linux
+        video = VideoReader(sprintf('%s/%s', video_root_path, filename));
+        
+        % This might not be accurate
+        video.FrameTotal = round( video.FrameRate*video.Duration);
+    end
     
     filenames = [filenames; filename];
-    width = [width; video.width];
-    height = [height; video.height];
-    totalDuration = [totalDuration; video.totalDuration];
-    nrFramesTotal = [nrFramesTotal; video.nrFramesTotal];
-    rate = [rate; video.rate]; 
+    width = [width; video.Width];
+    height = [height; video.Height];
+    totalDuration = [totalDuration; video.Duration];
+    nrFramesTotal = [nrFramesTotal; video.FrameTotal];
+    rate = [rate; video.FrameRate]; 
 end
 
 fprintf('stat:\n')
@@ -56,4 +69,3 @@ fprintf('[# of frames] total: %d, avg: %.3f, min: %d, max: %d\n', sum(nrFramesTo
 fprintf('[frame rate] avg: %.3f, min: %.3f, max: %.3f\n', mean(rate) , min(rate), max(rate));
 
 save('video_info.mat', 'filenames', 'width', 'height', 'totalDuration', 'nrFramesTotal', 'rate');
-
